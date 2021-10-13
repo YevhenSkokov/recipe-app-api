5c359c257fa6dcf43d7ba85b939f6da7cc121623
Yevhen Skokov <yevhen.skokov@anvileight.com>
2021-10-13 17:47:03 +0300

    Added tests to Ingridient api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -47,3 +47,11 @@ class ModelsTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredients_str(self):
+        """Test the ingredient representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
